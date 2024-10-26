e32fc3705895177dd8782ce04ff42ab3d3dfcc24
Prabhat <highkumar0@gmail.com>
2024-10-26 14:06:06 +0530

    created a Temperature application

---
+++ pages/js/temperature.js
@@ -0,0 +1,75 @@
+let input = document.querySelector("input");
+let select1 = document.querySelector("#form");
+let select2 = document.querySelector("#to");
+let button = document.querySelector("button");
+let inputNumber = document.querySelector(".fnum");
+let calNumber = document.querySelector(".lnum");
+let formValue = inputNumber.nextElementSibling;
+let toValue = calNumber.nextElementSibling;
+let inputNumValue = 0;
+let formSelection = "";
+let toSelection = "";
+function cal(form, to) {
+  if (form == "Fahrenheit" && to == "Celsius") {
+    return (input) => ((input - 32) * 5) / 9;
+  }
+  if (form == "Celsius" && to == "Fahrenheit") {
+    return (input) => (input * 9) / 5 + 32;
+  }
+
+  if (form == "Celsius" && to == "Kelvin") {
+    return (input) => input + 273.15;
+  }
+  if (form == "Kelvin" && to == "Celsius") {
+    return (input) => input - 273.15;
+  }
+  if (form == "Fahrenheit" && to == "Kelvin") {
+    return (input) => ((input - 32) * 5) / 9 + 273.15;
+  }
+  if (form == "Kelvin" && to == "Fahrenheit") {
+    return (input) => ((input - 273.15) * 9) / 5 + 32;
+  }
+}
+let final;
+function FetAddCalculated() {
+  if (formSelection && toSelection) {
+    final = cal(formSelection, toSelection);
+  }
+}
+function btnAllow() {
+  if (formSelection && toSelection && inputNumValue) {
+    button.style.cursor = "pointer";
+    button.disabled = false;
+    button.addEventListener("click", (e) => {
+      calNumber.parentElement.style.display = "block";
+      toValue.style.display = "inline-block";
+      let calculatedNum = final(parseFloat(inputNumValue));
+      calNumber.innerHTML = calculatedNum.toFixed(2);
+      inputNumber.innerHTML = inputNumValue;
+      formValue.innerHTML = formSelection;
+      toValue.innerHTML = toSelection;
+    });
+  }
+
+  if (formSelection === toSelection) {
+    button.style.cursor = " not-allowed";
+    button.disabled = true;
+  }
+}
+input.addEventListener("input", (e) => {
+  inputNumValue = e.target.value;
+  FetAddCalculated();
+  btnAllow();
+});
+
+select1.addEventListener("input", (e) => {
+  formSelection = e.target.value;
+  FetAddCalculated();
+  btnAllow();
+});
+select2.addEventListener("input", (e) => {
+  toSelection = e.target.value;
+
+  FetAddCalculated();
+  btnAllow();
+});
